1efaaec2c9c202fb99f0994c1677ca5507bfd1f4
chloe3c <chloe3c@209-3>
2024-12-18 12:45:28 +0200

    Добавьте файлы проекта.

---
+++ LABA3.2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите натуральное число n (n >= 10): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 10)
+{
+    Console.WriteLine("Число должно быть не менее 10.");
+    return;
+}
+
+Console.WriteLine("Нечетные кратные 5 числа от 10 до {0}:", n);
+
+for (int i = 11; i <= n; i += 2)
+{
+    if (i % 5 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
